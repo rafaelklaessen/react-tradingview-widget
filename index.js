20a3bb7beeee1df5a4568ee9f9e177ec2f3c6524
yangga <yangga0070@gmail.com>
2018-04-12 09:52:07 +0900

    added space

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-if(typeof window === 'undefined'){
+if (typeof window === 'undefined') {
     global.window = {}
 }
 
 require('./dist/manifest');
 
-if(typeof webpackJsonpreact_tradingview_widget === 'undefined'){
+if (typeof webpackJsonpreact_tradingview_widget === 'undefined') {
     global.webpackJsonpreact_tradingview_widget = window["webpackJsonpreact_tradingview_widget"]
 }
 
