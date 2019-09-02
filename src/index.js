0ff7f116d2665157291d9b4717eba9afd3d94aeb
Samuel Rousseau <samuelrb@dotmedias.com>
2019-09-02 17:06:14 -0400

    Début de la création des multiples widgets

--- src/index.js
+++ src/index.js
@@ -1,38 +1,39 @@
-import React, { PureComponent } from 'react';
-import PropTypes from 'prop-types';
+import React, { PureComponent } from "react"
+import PropTypes from "prop-types"
 
 export const BarStyles = {
-  BARS: '0',
-  CANDLES: '1',
-  HOLLOW_CANDLES: '9',
-  HEIKIN_ASHI: '8',
-  LINE: '2',
-  AREA: '3',
-  RENKO: '4',
-  LINE_BREAK: '7',
-  KAGI: '5',
-  POINT_AND_FIGURE: '6'
-};
+  BARS: "0",
+  CANDLES: "1",
+  HOLLOW_CANDLES: "9",
+  HEIKIN_ASHI: "8",
+  LINE: "2",
+  AREA: "3",
+  RENKO: "4",
+  LINE_BREAK: "7",
+  KAGI: "5",
+  POINT_AND_FIGURE: "6"
+}
 
 export const IntervalTypes = {
-  D: 'D',
-  W: 'W'
-};
+  D: "D",
+  W: "W"
+}
 
 export const RangeTypes = {
-  YTD: 'ytd',
-  ALL: 'all'
-};
+  YTD: "ytd",
+  ALL: "all"
+}
 
 export const Themes = {
-  LIGHT: 'Light',
-  DARK: 'Dark'
-};
+  LIGHT: "Light",
+  DARK: "Dark"
+}
 
-const SCRIPT_ID = 'tradingview-widget-script';
-const CONTAINER_ID = 'tradingview-widget';
+export class TradingViewStockChartWidget extends TradingViewWidget {
+  constructor(props) {
+    super(props, "https://s3.tradingview.com/tv.js")
+  }
 
-export default class TradingViewWidget extends PureComponent {
   static propTypes = {
     allow_symbol_change: PropTypes.bool,
     autosize: PropTypes.bool,
@@ -54,14 +55,14 @@ export default class TradingViewWidget extends PureComponent {
       60,
       120,
       180,
-      '1',
-      '3',
-      '5',
-      '15',
-      '30',
-      '60',
-      '120',
-      '180',
+      "1",
+      "3",
+      "5",
+      "15",
+      "30",
+      "60",
+      "120",
+      "180",
       IntervalTypes.D,
       IntervalTypes.W
     ]),
@@ -71,14 +72,14 @@ export default class TradingViewWidget extends PureComponent {
     popup_height: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
     popup_width: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
     range: PropTypes.oneOf([
-      '1d',
-      '5d',
-      '1m',
-      '3m',
-      '6m',
+      "1d",
+      "5d",
+      "1m",
+      "3m",
+      "6m",
       RangeTypes.YTD,
-      '12m',
-      '60m',
+      "12m",
+      "60m",
       RangeTypes.ALL
     ]),
     referral_id: PropTypes.string,
@@ -105,7 +106,7 @@ export default class TradingViewWidget extends PureComponent {
     widgetType: PropTypes.string,
     width: PropTypes.number,
     withdateranges: PropTypes.bool
-  };
+  }
 
   static defaultProps = {
     allow_symbol_change: true,
@@ -117,112 +118,130 @@ export default class TradingViewWidget extends PureComponent {
     hide_side_toolbar: true,
     hide_top_toolbar: false,
     interval: IntervalTypes.D,
-    locale: 'en',
+    locale: "en",
     save_image: true,
     show_popup_button: false,
     style: BarStyles.CANDLES,
     theme: Themes.LIGHT,
-    timezone: 'Etc/UTC',
-    toolbar_bg: '#F1F3F6',
-    widgetType: 'widget',
+    timezone: "Etc/UTC",
+    toolbar_bg: "#F1F3F6",
+    widgetType: "widget",
     width: 980,
     withdateranges: false
-  };
+  }
+}
 
-  containerId = `${CONTAINER_ID}-${Math.random()}`;
+const SCRIPT_ID = "tradingview-widget-script"
+const CONTAINER_ID = "tradingview-widget"
 
-  componentDidMount = () => this.appendScript(this.initWidget);
+class TradingViewWidget extends PureComponent {
+  script = ""
+  containerId = `${CONTAINER_ID}-${Math.random()}`
+
+  constructor(props, scriptUrl) {
+    super(props)
+    this.script = scriptUrl
+  }
+
+  componentDidMount = () => this.appendScript(this.initWidget)
 
   componentDidUpdate = () => {
-    this.cleanWidget();
-    this.initWidget();
-  };
+    this.cleanWidget()
+    this.initWidget()
+  }
 
-  canUseDOM = () => !!(
-    typeof window !== 'undefined' &&
-    window.document &&
-    window.document.createElement
-  );
+  canUseDOM = () => {
+    !!(
+      typeof window !== "undefined" &&
+      window.document &&
+      window.document.createElement
+    )
+  }
 
-  appendScript = (onload) => {
+  appendScript = onload => {
     if (!this.canUseDOM()) {
-      onload();
-      return;
+      onload()
+      return
     }
 
     if (this.scriptExists()) {
       /* global TradingView */
-      if (typeof TradingView === 'undefined') {
-        this.updateOnloadListener(onload);
-        return;
+      if (typeof TradingView === "undefined") {
+        this.updateOnloadListener(onload)
+        return
       }
-      onload();
-      return;
+      onload()
+      return
     }
-    const script = document.createElement('script');
-    script.id = SCRIPT_ID;
-    script.type = 'text/javascript';
-    script.async = true;
-    script.src = 'https://s3.tradingview.com/tv.js';
-    script.onload = onload;
-    document.getElementsByTagName('head')[0].appendChild(script);
-  };
 
-  getScriptElement = () =>
-    document.getElementById(SCRIPT_ID);
+    const script = document.createElement("script")
 
-  scriptExists = () =>
-    this.getScriptElement() !== null;
+    script.id = SCRIPT_ID
+    script.type = "text/javascript"
+    script.async = true
+    script.src = this.script
+    script.onload = onload
 
-  updateOnloadListener = (onload) => {
-    const script = this.getScriptElement();
-    const oldOnload = script.onload;
-    return script.onload = () => {
-      oldOnload();
-      onload();
-    };
-  };
+    document.getElementsByTagName("head")[0].appendChild(script)
+  }
+
+  getScriptElement = () => document.getElementById(SCRIPT_ID)
+
+  scriptExists = () => this.getScriptElement() !== null
+
+  updateOnloadListener = onload => {
+    const script = this.getScriptElement()
+    const oldOnload = script.onload
+    return (script.onload = () => {
+      oldOnload()
+      onload()
+    })
+  }
 
   initWidget = () => {
     /* global TradingView */
-    if (typeof TradingView === 'undefined' || !document.getElementById(this.containerId)) return;
+    if (
+      typeof TradingView === "undefined" ||
+      !document.getElementById(this.containerId)
+    )
+      return
 
-    const { widgetType, ...widgetConfig } = this.props;
-    const config = { ...widgetConfig, container_id: this.containerId };
+    const { widgetType, ...widgetConfig } = this.props
+    const config = { ...widgetConfig, container_id: this.containerId }
 
     if (config.autosize) {
-      delete config.width;
-      delete config.height;
+      delete config.width
+      delete config.height
     }
 
-    if (typeof config.interval === 'number') {
-      config.interval = config.interval.toString();
+    if (typeof config.interval === "number") {
+      config.interval = config.interval.toString()
     }
 
-    if (config.popup_width && typeof config.popup_width === 'number') {
-      config.popup_width = config.popup_width.toString();
+    if (config.popup_width && typeof config.popup_width === "number") {
+      config.popup_width = config.popup_width.toString()
     }
 
-    if (config.popup_height && typeof config.popup_height === 'number') {
-      config.popup_height = config.popup_height.toString();
+    if (config.popup_height && typeof config.popup_height === "number") {
+      config.popup_height = config.popup_height.toString()
     }
 
     /* global TradingView */
-    new TradingView[widgetType](config);
-  };
+    new TradingView[widgetType](config)
+  }
 
   cleanWidget = () => {
-    if (!this.canUseDOM()) return;
-    document.getElementById(this.containerId).innerHTML = '';
-  };
+    if (!this.canUseDOM()) return
+    document.getElementById(this.containerId).innerHTML = ""
+  }
 
   getStyle = () => {
-    if (!this.props.autosize) return {};
+    if (!this.props.autosize) return {}
     return {
-      width: '100%',
-      height: '100%'
-    };
-  };
+      width: "100%",
+      height: "100%"
+    }
+  }
 
   render = () => <article id={this.containerId} style={this.getStyle()} />
 }
--- src/index.test.js
+++ src/index.test.js
@@ -1,22 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import TradingViewWidget, {
+import React from "react"
+import ReactDOM from "react-dom"
+import {
+  TradingViewStockChartWidget,
   BarStyles,
   IntervalTypes,
   RangeTypes,
   Themes
-} from './index';
+} from "./index"
 
-describe('<TradingViewWidget />', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<TradingViewWidget symbol="NASDAQ:AAPL" />, div);
-  });
+describe("<TradingViewStockChartWidget />", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div")
+    ReactDOM.render(<TradingViewStockChartWidget symbol="NASDAQ:AAPL" />, div)
+  })
 
-  it('exports constants', () => {
-    expect(typeof BarStyles).toBe('object');
-    expect(typeof IntervalTypes).toBe('object');
-    expect(typeof RangeTypes).toBe('object');
-    expect(typeof Themes).toBe('object');
-  });
-});
+  it("exports constants", () => {
+    expect(typeof BarStyles).toBe("object")
+    expect(typeof IntervalTypes).toBe("object")
+    expect(typeof RangeTypes).toBe("object")
+    expect(typeof Themes).toBe("object")
+  })
+})
